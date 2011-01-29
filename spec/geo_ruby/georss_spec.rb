4559e0e25e30edacd8f634bfc3123fdafbe9b82f
Marcos Piccinini <x@nofxx.com>
2011-01-29 04:43:02 -0200

    some rss test subjects

--- spec/geo_ruby/georss_spec.rb
+++ spec/geo_ruby/georss_spec.rb
@@ -1,6 +1,14 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
+RSS_DATA_DIR = File.dirname(__FILE__) + '/../data/georss/'
+
 describe GeorssParser do
+
+  it "should parse an rss file" do
+    geo = GeorssParser.new.parse(File.read(RSS_DATA_DIR + "/w3c.xml"))
+    geo.should be_a Point
+  end
+
 
 
 end
