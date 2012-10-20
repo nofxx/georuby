1f28310f6df2cdfa668875c7b879a72486828556
Keenan Brock <keenan.brock@ping4.com>
2012-10-19 23:17:10 -0400

    requiring geo ruby no longer requires all dependent gems

--- lib/geo_ruby.rb
+++ lib/geo_ruby.rb
@@ -1,24 +1,10 @@
 # $:.unshift(File.dirname(__FILE__)) #unless
 #  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
-require 'geo_ruby/simple_features/helper'
-require 'geo_ruby/simple_features/ewkt_parser'
-require 'geo_ruby/simple_features/ewkb_parser'
-require 'geo_ruby/simple_features/geometry'
-require 'geo_ruby/simple_features/point'
-require 'geo_ruby/simple_features/line_string'
-require 'geo_ruby/simple_features/linear_ring'
-require 'geo_ruby/simple_features/polygon'
-require 'geo_ruby/simple_features/multi_point'
-require 'geo_ruby/simple_features/multi_line_string'
-require 'geo_ruby/simple_features/multi_polygon'
-require 'geo_ruby/simple_features/geometry_collection'
-require 'geo_ruby/simple_features/envelope'
-require 'geo_ruby/simple_features/geometry_factory'
-
+require 'geo_ruby/simple_features'
 # Require if you need
-require 'geo_ruby/shp4r/shp'
-require 'geo_ruby/gpx4r/gpx'
-require 'geo_ruby/geojson'
-require 'geo_ruby/georss'
-require 'geo_ruby/kml'
+# require 'geo_ruby/shp4r/shp'
+# require 'geo_ruby/gpx4r/gpx'
+# require 'geo_ruby/geojson'
+# require 'geo_ruby/georss'
+# require 'geo_ruby/kml'
